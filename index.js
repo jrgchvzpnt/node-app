594f6feb92bbaa6cb645856c57dce6ada9b8dc5a
Jorge Ernesto Chavez Puente <110014897+jrgchvzpnt@users.noreply.github.com>
2025-07-15 20:15:33 -0700

    Create index.js
    commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express'):
+const app = express();
+app.get('/', (req, res) => res.send('!Hola, Mundo!'));
+app.listen(3000, () => console.log('Servidor ejecutandose en el puerto 3000'));
